65798acbbe039dbfb60f70e396b67f4badd37ed8
Md. Ahsanul Haque Riad <riadk910@gmail.com>
2024-12-20 21:11:36 +0600

    Add files via upload

---
+++ JavaScript/1Basics.js
@@ -0,0 +1,19 @@
+console.log("Riad");
+console.log("Tis is my first javascript Code");
+
+//How to declare variable
+let a = 20;
+let b= 15;
+console.log("Sum is =", a+b);
+
+let pencilPrice =10;
+let penPrice = 5;
+
+// console.log("The total price is :", pencilPrice+penPrice, "TK");
+
+let output = "The total price is :" +(pencilPrice+penPrice)+"TK"
+console.log(output);
+
+//ANOTHER WAY TO PRINT OUTPUT
+let out =`The total price is = ${pencilPrice + penPrice} Taka`;
+console.log(out);
