4866927dd06e73eed8795ebf4d0dae9a525a7d9a
Md. Ahsanul Haque Riad <riadk910@gmail.com>
2024-12-21 18:15:02 +0600

    Add files via upload

---
+++ JavaScript/2Basics.js
@@ -0,0 +1,9 @@
+//ALERT DISPLAYS AN ALERT MESSAGE ON THE PAGE
+// alert("Something is wrong!");
+// console.error("This is an error message");
+
+//PROMPT DISPLAYS A DIALOG BOX THAT ASKS USER FOR SOME INPUT
+// prompt("Enter your name :");
+let firstName = prompt("Enter your first name:");
+let lastName = prompt("Enter your last name:");
+console.log("Welcome", firstName,lastName, "!");
