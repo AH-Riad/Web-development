35ccb6a4bf047c26775fb8612db90b07acf9eec0
Md. Ahsanul Haque Riad <riadk910@gmail.com>
2024-12-30 13:16:00 +0600

    Add files via upload

---
+++ JavaScript/6time.js
@@ -0,0 +1,17 @@
+console.log("Hi there! ");
+
+
+
+//setTimeout function     NO NEED TO CALL
+setTimeout(()=>{
+    console.log("Riad is learning javaScript")
+}, 2000);
+
+
+//setInterval function
+
+let id = setInterval(()=>{
+    console.log("I wish to buy my dream bike with in 2025")
+},3000);
+
+console.log(id);
